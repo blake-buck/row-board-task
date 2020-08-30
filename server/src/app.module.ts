c6f01c4b9a2b6b6db4d387a747fc4ce57e540ef4
blake-buck <blakemanbuck@gmail.com>
2020-08-30 12:48:46 -0500

    add rate limiting to routes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer  } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
@@ -8,6 +8,10 @@ import {join} from 'path';
 import { CognitoModule } from './cognito/cognito.module';
 import { LoggerModule } from './logger/logger.module';
 import { DataModule } from './data/data.module';
+
+import * as rateLimit from 'express-rate-limit';
+import { DataController } from './data/data.controller';
+import { CognitoController } from './cognito/cognito.controller';
 
 @Module({
   imports: [
@@ -34,4 +38,16 @@ import { DataModule } from './data/data.module';
     AppService
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer:MiddlewareConsumer){
+    // limit of 150 operations every 30 minutes for the DataController routes
+    consumer
+      .apply(rateLimit({windowMs: 1000 * 60 * 30, max:150}))
+      .forRoutes(DataController)
+    
+    consumer
+      .apply(rateLimit({windowMs: 1000 * 60 * 60, max: 10}))
+      .forRoutes(CognitoController)
+  }
+
+}
