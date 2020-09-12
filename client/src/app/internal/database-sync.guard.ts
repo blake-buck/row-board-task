1f12e2bc59266bc2f667e69445906314c1626a97
blake-buck <blakemanbuck@gmail.com>
2020-09-11 21:02:27 -0500

    added an expired token check before database sync call

--- client/src/app/internal/database-sync.guard.ts
+++ client/src/app/internal/database-sync.guard.ts
@@ -1,17 +1,28 @@
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { retrieveStateFromDb, getState } from '../store/app.actions';
 import { AppStore } from '../store/app.state';
+import { AppService } from '../store/app.service';
+import { ApiResult } from '../store/app.effects';
 
 @Injectable({
     providedIn:'root'
 })
 
 export class DatabaseSyncGuard implements CanActivate{
-    constructor(private store:Store<AppStore>){}
+    constructor(private store:Store<AppStore>, private service:AppService, private router:Router){}
 
-    canActivate(){
+    async canActivate(){
+        if(this.service.isAccessTokenExpired()){
+            try{
+                const result:ApiResult = await this.service.refresh().toPromise<any>()
+                localStorage.setItem('accessToken', result.message.AuthenticationResult.AccessToken)
+            }
+            catch(e){
+                return this.router.parseUrl('/');
+            }
+        }
         // initialize state in app
         this.store.dispatch(getState())
         // retrieve state from DB
