5aad2c53acd9d03753fa1d58e8e78958030eca3d
blake-buck <blakemanbuck@gmail.com>
2020-09-13 09:55:18 -0500

    http headers get updated before requests

--- client/src/app/headers-interceptor.ts
+++ client/src/app/headers-interceptor.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor } from '@angular/common/http';
+import { AppService } from './store/app.service';
+import { first, map } from 'rxjs/operators';
 
 @Injectable()
 export class HeadersInterceptor implements HttpInterceptor{
+    constructor(private service:AppService){}
+    
     intercept(req, next){
         if(localStorage.getItem('accessToken')){
+            if(this.service.isAccessTokenExpired()){
+                return this.service.refresh().pipe(
+                    first(),
+                    map((result:any) => {
+                        const accessToken = result.message.AuthenticationResult.RefreshToken;
+                        this.service.addTokensToStorage({accessToken});
+                        const updatedHeaders = req.clone({
+                            headers: req.headers.set('jwt', accessToken)
+                        })
+                        return next.handle(updatedHeaders);
+                    })
+                )
+            }
+
             const updatedHeaders = req.clone({
                 headers: req.headers.set('jwt', localStorage.getItem('accessToken'))
             })
