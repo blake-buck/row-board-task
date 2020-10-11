585bd93e190fda26b8721c8059c402bf7b7c3c4e
blake-buck <blakemanbuck@gmail.com>
2020-10-11 16:39:19 -0500

    fixed label-size error on firefox

--- client/src/app/internal/util/label-size.pipe.ts
+++ client/src/app/internal/util/label-size.pipe.ts
@@ -8,7 +8,7 @@ export class LabelSizePipe implements PipeTransform{
     transform(textLength){
         // Firefox and chrome textareas have different methods of sizing text areas
         if(window.navigator.userAgent.includes('Firefox')){
-            return textLength;
+            return textLength ? textLength : 1;
         }
         return textLength  > 1 ? textLength -1 : 1
     }
